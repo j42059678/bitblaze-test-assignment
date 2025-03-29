a8f73002d6653576fe2ffd289af408c02a18e969
x <lambda@x.org>
2025-03-29 15:01:26 +0000

    up

--- src/scripts/sh/lvm/group/create-group.sh
+++ src/scripts/sh/lvm/group/create-group.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+set -e
+
+source get-group-by-id.sh
 
 function create_group() {
     VG_ID="$1"
     DRIVES=${@:2}
     vgcreate ${VG_ID} ${DRIVES}
 }
-
-create_group $@
+VG_ID="$1"
+create_group "${VG_ID}"
+get_group_by_id "${VG_ID}"
--- src/scripts/sh/lvm/group/get-group-by-id.sh
+++ src/scripts/sh/lvm/group/get-group-by-id.sh
@@ -2,7 +2,7 @@
 
 function get_group_by_id() {
     VG_ID="$1"
-    vgs "${VG_ID}"
+    vgs "${VG_ID}" | awk -f src/scripts/awk/table2json.awk
 }
 
 get_group_by_id $@
