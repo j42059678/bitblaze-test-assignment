1aa512bacaf5729e6c0328b9b0ffd115cecbaaeb
x <lambda@x.org>
2025-03-30 17:08:57 +0000

    up

--- src/scripts/sh/main.sh
+++ src/scripts/sh/main.sh
@@ -4,6 +4,6 @@ for script in $SCRIPTS_DIR/sh/drive/*.sh; do
     source "$script"
 done
 
-for script in $SCRIPTS_DIR/lvm/**/*.sh; do
+for script in $SCRIPTS_DIR/sh/lvm/**/*.sh; do
     source "$script"
 done
