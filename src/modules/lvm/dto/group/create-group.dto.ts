a70c89ba81128594988315492bd84eb07a406824
x <lambda@x.org>
2025-03-30 07:46:43 +0000

    up

--- src/modules/lvm/dto/group/create-group.dto.ts
+++ src/modules/lvm/dto/group/create-group.dto.ts
@@ -1,12 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { isString } from 'class-validator';
+import { IsArray, IsNotEmpty } from 'class-validator';
 import { IDto } from 'src/modules/base/base.dto';
 
 export class CreateGroupDto implements IDto {
   @ApiProperty({ description: 'The name of the group' })
-  @isString()
+  @IsNotEmpty()
   name: string
   @ApiProperty({ description: 'The array of drive ids' })
-  @isString()
+  @IsArray()
+  @IsNotEmpty()
   drives: string[]
 }
