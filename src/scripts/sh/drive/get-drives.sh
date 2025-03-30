f4080eff3a9dcc7851b2f674904ece7def2a3421
x <lambda@x.org>
2025-03-30 07:10:15 +0000

    up

--- src/scripts/sh/drive/get-drives.sh
+++ src/scripts/sh/drive/get-drives.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 function get_drives() {
-     lsblk -b -o NAME,SIZE | awk -f src/scripts/awk/table2json.awk | jq 'map({name: .NAME, size: .SIZE})'
+     lsblk -b -o NAME,SIZE | awk -f src/scripts/awk/table2json.awk | jq '{drives: map({name: .NAME, size: .SIZE})}'
 }
 
 get_drives
--- src/scripts/sh/lvm/group/get-groups.sh
+++ src/scripts/sh/lvm/group/get-groups.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 function get_groups() {
-    vgs | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq 'map({id: .VG})'
+    vgs | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq '{groups: map({id: .VG})}'
 }
 
 get_groups
