d7e7bd272c3859880597bcedd43384bea79b1e51
x <lambda@x.org>
2025-03-30 07:51:01 +0000

    up

--- src/scripts/sh/lvm/group/create-group.sh
+++ src/scripts/sh/lvm/group/create-group.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 set -e
 
+source $SCRIPTS_DIR/sh/lvm/group/get-group-by-id.sh
+
 function create_group() {
     VG_ID="$1"
     DRIVES=${@:2}
     vgcreate ${VG_ID} ${DRIVES} &> /dev/null
 }
 JSON=$1
-VG_ID=$(echo "${JSON}" | jq '.name')
-DRIVES=$(echo "${JSON}" | jq '.drives | join(" ")')
+echo "${JSON}" && exit
+VG_ID=$(echo "${JSON}" | jq -r '.name')
+DRIVES=$(echo "${JSON}" | jq -r '.drives | join(" ")')
 create_group "${VG_ID}" ${DRIVES}
 get_group_by_id "${VG_ID}"
--- src/scripts/sh/lvm/group/get-group-by-id.sh
+++ src/scripts/sh/lvm/group/get-group-by-id.sh
@@ -5,4 +5,5 @@ function get_group_by_id() {
     vgs --units b "${VG_ID}" | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq 'map({id: .VG, size: .VSize, free: .VFree})[0]'
 }
 
-get_group_by_id $@
+VG_ID=$1
+get_group_by_id $VG_ID
