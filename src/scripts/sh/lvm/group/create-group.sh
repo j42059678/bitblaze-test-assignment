cd45eceeea38ad936bda16fe2a8e482bb8ad6cdb
x <lambda@x.org>
2025-03-30 08:21:00 +0000

    up

--- src/scripts/sh/lvm/group/create-group.sh
+++ src/scripts/sh/lvm/group/create-group.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 function create_group() {
-    $JSON=$1
+    JSON=$1
     VG_ID=$(echo "${JSON}" | jq -r '.name')
     DRIVES=$(echo "${JSON}" | jq -r '.drives | join(" ")')
-    vgcreate ${VG_ID} ${DRIVES}
+    echo $JSON
 }
