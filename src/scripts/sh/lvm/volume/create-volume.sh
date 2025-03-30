8d68d55080680c6fb54b3d432283b0e6ed4f5ca9
x <lambda@x.org>
2025-03-30 10:01:59 +0000

    up

--- src/scripts/sh/lvm/volume/create-volume.sh
+++ src/scripts/sh/lvm/volume/create-volume.sh
@@ -4,7 +4,7 @@ function create_volume() {
     JSON=$1
     LV_ID=$(echo "${JSON}" | jq -r '.name')
     VG_ID=$(echo "${JSON}" | jq -r '.vg_id')
-    VG_SIZE=$(echo "${JSON}" | jq -r '.size')
+    LV_SIZE=$(echo "${JSON}" | jq -r '.size')
     lvcreate -L "${LV_SIZE}B" -n "${LV_ID}" "${VG_ID}"
     echo $LV_ID
 }
