f81c709cd19364cc39a6a5fa66e895c6bcfe14bd
x <lambda@x.org>
2025-03-30 11:45:46 +0000

    up

--- src/scripts/sh/lvm/volume/delete-volume-by-group-id-and-volume-id.sh
+++ src/scripts/sh/lvm/volume/delete-volume-by-group-id-and-volume-id.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 function delete_volume_by_group_id_and_volume_id() {
-    LV_ID=$1
-    lvs --units b $LV_ID | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq '{volumes: map({id: .LV, vg_id: .VG, size: .LSize})}'
+    VG_ID=$1
+    LV_ID=$2
+    vgremove $VG_ID -f $LV_ID
+    echo -n $VG_ID $LV_ID
 }
