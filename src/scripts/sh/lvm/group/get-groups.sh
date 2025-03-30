52ee7aa5e62a4f4f794006bcd7fd07be9aaeea24
x <lambda@x.org>
2025-03-30 10:53:39 +0000

    up

--- src/scripts/sh/lvm/group/get-groups.sh
+++ src/scripts/sh/lvm/group/get-groups.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 function get_groups() {
-    vgs --units b | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq '{groups: map({id: .VG, size: tonumber(.VSize[0:-1]), free: tonumber(.VFree[0:-1])})}'
+    vgs --units b | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq '{groups: map({id: .VG, size: .VSize[0:-1] |= tonumber, free: .VFree[0:-1] |= tonumber})}'
 }
