49cef4f9ba9539984074477699970cdbcbe8ac29
x <lambda@x.org>
2025-03-30 08:43:05 +0000

    up

--- src/modules/lvm/dto/volume/create-volume.dto.ts
+++ src/modules/lvm/dto/volume/create-volume.dto.ts
@@ -1,11 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 import { IDto } from 'src/modules/base/base.dto';
 
 export class CreateVolumeDto implements IDto {
   @ApiProperty({ description: 'The name of the volume' })
+  @IsNotEmpty()
   name: string;
   @ApiProperty({ description: 'The id of the group' })
+  @IsNotEmpty()
   vg_id: string;
   @ApiProperty({ description: 'The size of the volume in bytes' })
+  @IsNotEmpty()
+  @IsNumber()
   size: number;
 }
--- src/modules/lvm/dto/volume/volume.list.dto.ts
+++ src/modules/lvm/dto/volume/volume.list.dto.ts
@@ -3,6 +3,7 @@ import { IDto } from 'src/modules/base/base.dto';
 export interface VolumeListItemDto extends IDto {
   id: string;
   vg: string;
+  size: number;
 }
 
 export interface VolumeListDto extends IDto {
