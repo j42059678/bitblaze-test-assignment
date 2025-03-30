463fde25362c2fe20452262152bb257a64f40bfd
x <lambda@x.org>
2025-03-30 08:44:53 +0000

    up

--- src/scripts/sh/drive/get-drive-by-id.sh
+++ src/scripts/sh/drive/get-drive-by-id.sh
@@ -2,5 +2,5 @@
 
 function get_drive_by_id() {
     DRIVE_ID=$1
-    lsblk -o NAME,SIZE,MOUNTPOINTS -b -Q 'MOUNTPOINTS !~ "/(boot|efi|home|usr)?$"' "/dev/{$DRIVE_ID}" | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq 'map({name: .NAME, size: .SIZE})[0]'
+    lsblk -o NAME,SIZE,MOUNTPOINTS -b -Q 'MOUNTPOINTS !~ "/(boot|efi|home|usr)?$"' "/dev/${DRIVE_ID}" | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq 'map({name: .NAME, size: .SIZE})[0]'
 }
