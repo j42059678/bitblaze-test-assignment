4c0cfd889fcfe769b51f9f1b6ec77c56080495ba
x <lambda@x.org>
2025-03-30 17:08:09 +0000

    up

--- src/modules/utils/service/executor.service.ts
+++ src/modules/utils/service/executor.service.ts
@@ -11,7 +11,7 @@ interface IExecutor {
 @Injectable()
 export class ExecutorService implements IExecutor {
   async execute(command: string): Promise<string> {
-    const { stdout, stderr } = await exec(`$SHELL -c ${process.env.SCRIPTS_DIR}/sh/main.sh '${command}'`);
+    const { stdout, stderr } = await exec(`$SHELL -c 'source ${process.env.SCRIPTS_DIR}/sh/main.sh; ${command}'`);
     if (stderr) {
       throw new Error(stderr);
     }
