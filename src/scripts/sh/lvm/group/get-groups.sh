81be94678296d9a7ed52a123258e27073734a646
x <lambda@x.org>
2025-03-30 07:01:02 +0000

    up

--- src/scripts/sh/lvm/group/get-groups.sh
+++ src/scripts/sh/lvm/group/get-groups.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 function get_groups() {
-    vgs | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq .
+    vgs | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq 'map({id: .VG})'
 }
 
 get_groups
