b2219ccec2927ebb6b43eee1eac626afac7bc1f5
x <lambda@x.org>
2025-03-30 10:51:03 +0000

    up

--- src/scripts/sh/lvm/volume/get-volumes.sh
+++ src/scripts/sh/lvm/volume/get-volumes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 function get_volumes() {
-    lvs --units b | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq '{volumes: map({id: .LV, vg_id: .VG, size: .LSize[0:-1]})}'
+    lvs --units b | awk -f ${SCRIPTS_DIR}/awk/table2json.awk | jq '{volumes: map({id: .LV, vg_id: .VG, size: tonumber(.LSize[0:-1])})}'
 }
