9ca8e446ff1d640e6201d31e00410c7a284e8439
x <lambda@x.org>
2025-03-30 12:02:45 +0000

    up

--- src/scripts/sh/lvm/volume/get-volumes-by-group-id.sh
+++ src/scripts/sh/lvm/volume/get-volumes-by-group-id.sh
@@ -4,5 +4,5 @@ source "${SCRIPTS_DIR}/sh/lvm/volume/get-volumes.sh"
 
 function get_volumes_by_group_id() {
     VG_ID="$1"
-    get_volumes | jq ".volumes[] | map(.vg_id == \"${VG_ID}\")"
+    get_volumes | jq "[.volumes[] | select(.vg_id == \"${VG_ID}\")]"
 }
