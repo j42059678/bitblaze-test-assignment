bbdaadcccbadf24cb41af8602373308a4f2c3f2e
x <lambda@x.org>
2025-03-30 11:43:36 +0000

    up

--- src/scripts/sh/lvm/volume/get-volume-by-group-id-and-volume-id.sh
+++ src/scripts/sh/lvm/volume/get-volume-by-group-id-and-volume-id.sh
@@ -5,5 +5,5 @@ source "${SCRIPTS_DIR}/sh/lvm/volume/get-volumes.sh"
 function get_volume_by_group_id_and_volume_id() {
     VG_ID="$1"
     LV_ID="$2"
-    get_volumes | jq ".volumes[] | select(.vg_id == \"${LV_ID}\" and .id == \"${LV_ID}\")"
+    get_volumes | jq ".volumes[] | select(.vg_id == \"${VG_ID}\" and .id == \"${LV_ID}\")"
 }
