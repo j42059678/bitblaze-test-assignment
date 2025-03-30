b335496b588be4e35f0880006100c11f668860c0
x <lambda@x.org>
2025-03-30 09:43:43 +0000

    up

--- src/scripts/sh/lvm/group/get-group-by-id.sh
+++ src/scripts/sh/lvm/group/get-group-by-id.sh
@@ -4,5 +4,5 @@ source ${SCRIPTS_DIR}/sh/lvm/group/get-groups.sh
 
 function get_group_by_id() {
     VG_ID="$1"
-    get_groups | jq ".groups[] | select(.id == '${VG_ID}')"
+    get_groups | jq ".groups[] | select(.id == \"${VG_ID}\")"
 }
--- src/scripts/sh/lvm/volume/get-volume-by-id.sh
+++ src/scripts/sh/lvm/volume/get-volume-by-id.sh
@@ -4,5 +4,5 @@ source ${SCRIPTS_DIR}/sh/lvm/volume/get-volumes.sh
 
 function get_volume_by_id() {
     $LV_ID=$1
-    get_volumes | jq ".groups[] | select(.id == '${LV_ID}')"
+    get_volumes | jq ".groups[] | select(.id == \"${LV_ID}\")"
 }
