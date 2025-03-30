2a04998cef5b536e3cbb0c56a6ea300df6e64d7c
x <lambda@x.org>
2025-03-30 09:28:44 +0000

    up

--- src/scripts/sh/drive/get-drive-by-id.sh
+++ src/scripts/sh/drive/get-drive-by-id.sh
@@ -4,5 +4,5 @@ source $SCRIPTS_DIR/sh/drive/get-drives.sh
 
 function get_drive_by_id() {
     DRIVE_ID=$1
-    get_drives | jq ".drives[] | select(.id == ${DRIVE_ID})"
+    get_drives | jq ".drives[] | select(.id == \"${DRIVE_ID}\")"
 }
