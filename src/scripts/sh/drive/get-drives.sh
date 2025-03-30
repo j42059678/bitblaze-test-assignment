bd39ed416c6adc405074d29220cee513b3e917d0
x <lambda@x.org>
2025-03-30 11:12:23 +0000

    up

--- src/scripts/sh/drive/get-drives.sh
+++ src/scripts/sh/drive/get-drives.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 function get_drives() {
-     lsblk -bdo NAME,SIZE -Q 'TYPE == "disk"' | grep -v "$(lsblk -d -nopkname $(findmnt -no source /))" | awk -f $SCRIPTS_DIR/awk/table2json.awk | jq '{drives: map({id: .NAME, size: .SIZE})}'
+     lsblk -bdo NAME,SIZE -Q 'TYPE == "disk"' | grep -v "$(lsblk -d -nopkname $(findmnt -no source /))" | awk -f $SCRIPTS_DIR/awk/table2json.awk | jq '{drives: map({id: .NAME, size: .SIZE | tonumber})}'
 }
